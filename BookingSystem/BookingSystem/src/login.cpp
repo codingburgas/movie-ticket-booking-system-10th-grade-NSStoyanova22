92bc5178aac5291fd2f657e0b18a76a2e8f23ab6
nsstoyanova22 <nsstoyanova22@codingburgas.bg>
2025-04-28 15:42:57 +0300

    Create basic login structure. Close #3

--- BookingSystem/BookingSystem/src/login.cpp
+++ BookingSystem/BookingSystem/src/login.cpp
@@ -1,7 +1,32 @@
 #include "../include/login.h"
 
 void Login::display() {
-
         system("CLS");
-    std::cout << "Welcome back !" << std::endl;
-}
+        std::cout << "+-----------------------------+\n";
+        std::cout << "|           Welcome !         |\n";
+        std::cout << "+-----------------------------+\n";
+
+        std::string email, password;
+
+
+       
+        std::cout << "Email: ";
+        getline(std::cin, email);
+        while (email.empty()) {
+            std::cout << "Email cannot be empty!\n";
+            std::cout << "Email: ";
+            getline(std::cin, email);
+        }
+
+        std::cout << "Password: ";
+        getline(std::cin, password);
+        while (password.empty()) {
+            std::cout << "Password cannot be empty!\n";
+            std::cout << "Password: ";
+            getline(std::cin, password);
+        }
+
+
+
+    }
+
