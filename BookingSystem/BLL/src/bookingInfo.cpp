5e23dea69c8c422179dcbfdaef2b9cecff105274
NSStoyanova22 <NSStoyanova22@codingburgas.bg>
2025-06-25 08:42:07 +0300

    PickMovie functionality finished

--- BookingSystem/BLL/src/bookingInfo.cpp
+++ BookingSystem/BLL/src/bookingInfo.cpp
@@ -4,5 +4,7 @@ namespace bookingInfo {
 	std::string city = "";
 	std::string cinema = "";
 	std::string movie = "";
-
+	std::string projectionDatetime = "";
+	std::string hall = "";
+	std::vector<Seat> selectedSeats;
 }
--- BookingSystem/BookingSystem/src/pickCinema.cpp
+++ BookingSystem/BookingSystem/src/pickCinema.cpp
@@ -1,64 +1,67 @@
 #include "../include/pickCinema.h"
 
-
-
 void pickCinema::display(PageHandler& pages) {
     system("CLS");
     std::cout << "Where would you like to watch a movie?\n";
+    std::cout << "--------------------------------------\n";
 
     ordered_json data = getCitiesData();
-    if (data.empty()) return;
+    if (data.empty()) {
+        std::cout << "Could not load cinema data.\n";
+        return;
+    }
 
-    
-    int cityIndex = 0;
+ 
     for (size_t i = 0; i < data["cities"].size(); ++i) {
         std::cout << i + 1 << " > " << data["cities"][i]["name"] << std::endl;
     }
+}
 
-  
+void pickCinema::actionHandler(PageHandler& pages) {
+    ordered_json data = getCitiesData();
+    if (data.empty()) return;
+
+    int cityIndex = 0;
     std::cout << "\nPick a city by number: ";
     std::cin >> cityIndex;
     cityIndex--; 
 
-    if (cityIndex < 0 || cityIndex >= data["cities"].size()) {
+    if (std::cin.fail() || cityIndex < 0 || cityIndex >= data["cities"].size()) {
         std::cout << "Invalid city choice.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        system("pause");
         return;
     }
 
-    
+  
     bookingInfo::city = data["cities"][cityIndex]["name"];
-    std::cout << "\n--- " << bookingInfo::city << " ---\n";
 
+    system("CLS");
+    std::cout << "--- Cinemas in " << bookingInfo::city << " ---\n";
 
     const auto& cinemas = data["cities"][cityIndex]["cinemas"];
     for (size_t i = 0; i < cinemas.size(); ++i) {
-        std::cout << i + 1 << " > " << cinemas[i] << std::endl;
+        std::cout << i + 1 << " > " << cinemas[i]["name"] << std::endl;
     }
 
- 
     int cinemaIndex = 0;
     std::cout << "\nPick a cinema by number: ";
     std::cin >> cinemaIndex;
     cinemaIndex--;
 
-    if (cinemaIndex < 0 || cinemaIndex >= cinemas.size()) {
+    if (std::cin.fail() || cinemaIndex < 0 || cinemaIndex >= cinemas.size()) {
         std::cout << "Invalid cinema choice.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        system("pause");
+       
+        bookingInfo::city = "";
         return;
     }
-    else {
-        pages.pickACinemaPageShouldDisplay = false;
-        pages.pickAMovieShouldDisplay = true;
-    }
-   
-    bookingInfo::cinema = cinemas[cinemaIndex].get<std::string>();
-    
-    
+
   
+    bookingInfo::cinema = cinemas[cinemaIndex]["name"];
+    pages.pickACinemaPageShouldDisplay = false;
+    pages.pickAMovieShouldDisplay = true;
 }
-
-
-void pickCinema::actionHandler(PageHandler& pages) {
-
-
-}
-
